57fe2772ec91e81a894c061c2792dcedd2928070
HWKIM <pottq577@naver.com>
2023-03-22 22:05:28 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("i run all the time");
+  // 두 개의 argument를 가지고, 첫 번째엔 한 번만 실행할 코드, 두 번쨰엔 []을 넣어줌
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
