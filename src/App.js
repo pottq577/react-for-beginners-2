564f5fb3af7df7fcb04efd7a7645c2a2cbb68018
HWKIM <pottq577@naver.com>
2023-03-24 23:27:11 +0900

    Movie App Part Two

--- src/App.js
+++ src/App.js
@@ -1,52 +1,5 @@
-import { useEffect, useState } from "react";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  /*
-  .then() 대신 await async를 사용하여 좀 더 짧고 보기 편한 코드로 대체 가능
-  useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, []);
- */
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
