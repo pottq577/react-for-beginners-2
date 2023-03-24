1df3bac6c7cd2de087afc42e86d2e296491a3158
HWKIM <pottq577@naver.com>
2023-03-24 23:46:51 +0900

    Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  // Switch는 Route(URL)를 찾으면 컴포넌트를 렌더링함
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
