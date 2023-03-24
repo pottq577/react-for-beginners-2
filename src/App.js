161ed193439ca812e66d396a1c305b05b8c4c21c
HWKIM <pottq577@naver.com>
2023-03-24 23:05:43 +0900

    Movie App Part One

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
 import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    getMovies();
+  }, []);
+  /*
+  .then() 대신 await async를 사용하여 좀 더 짧고 보기 편한 코드로 대체 가능
+  useEffect(() => {
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
       .then((response) => response.json())
       .then((json) => {
-        setCoins(json);
+        setMovies(json.data.movies);
         setLoading(false);
       });
   }, []);
+ */
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <ul>
-          {coins.map((coin) => (
-            <li>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </li>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
 }
-/**
- * !! CODE CHALLENGE
- * 사용자에게 가진 돈을 입력받으면 그 돈으로 얼마의 BTC를 살 수 있는지 계산하는 기능 구현
- */
+
 export default App;
